8378d4e0aa63a6b99fef1fcee081930d52ce6391
zoechi <gzoechi@gmail.com>
2014-01-31 13:09:50 +0100

    #4 add logging fix class assignment

--- lib/collapse.dart
+++ lib/collapse.dart
@@ -9,6 +9,9 @@ import "package:angular/angular.dart";
 import "package:angular/utils.dart";
 import 'transition.dart';
 
+import 'package:logging/logging.dart' show Logger;
+final _log = new Logger('angular.ui.collapse');
+
 /**
  * Collapse Module.
  */
@@ -20,11 +23,11 @@ class CollapseModule extends Module {
 }
 
 /**
- * Collapse directive.
+ * The collapsible directive indicates a block of html that will expand and collapse.
  */
 @NgDirective(selector:'[collapse]')
 class Collapse {
-  
+
   @NgOneWay("collapse")
   void set collapseAttr(bool value) {
     if (toBool(value)) {
@@ -33,71 +36,85 @@ class Collapse {
       expand();
     }
   }
-  
+
   dom.Element element;
   Transition transition;
   Scope scope;
-  
+
   var initialAnimSkip = true;
   async.Completer currentTransition;
-  
-  Collapse(this.element, this.transition, this.scope);
+
+  Collapse(this.element, this.transition, this.scope) {
+    _log.fine('CollapseDirective');
+  }
 
   async.Future doTransition(change) {
     async.Completer newTransition = transition(element, change);
-    
+
     var newTransitionDone = () {
       // Make sure it's this transition, otherwise, leave it alone.
       if (currentTransition == newTransition) {
         currentTransition = null;
       }
     };
-    
+
     if (currentTransition != null && !currentTransition.isCompleted) {
       currentTransition.completeError('Canceled');
     }
     currentTransition = newTransition;
-    newTransition.future.then((value)=> newTransitionDone(), onError:(e)=>newTransitionDone());
+    newTransition.future.then((value)=> newTransitionDone(), onError:(e) => newTransitionDone());
     return newTransition.future;
   }
-  
+
   void expand() {
     if (initialAnimSkip) {
       initialAnimSkip = false;
       expandDone();
     } else {
-      element.classes.remove('collapse');
-      element.classes.add('collapsing');
-      doTransition({ 'height': '${element.scrollHeight}px' }).then((value)=>expandDone());
+      element.classes
+        ..remove('collapse')
+        ..add('collapsing');
+      doTransition({ 'height': '${element.children[0].scrollHeight}px' }).then((value) => expandDone(), onError: (e) {
+        _log.fine('Error on expand: ${e}');
+        expandDone();
+      });
     }
   }
-  
+
   void expandDone() {
-    element.classes.remove('collapsing');
-    element.classes.add('collapse-in');
+    element.classes
+      ..remove('collapsing')
+      ..add('in')
+      ..add('collapse');
     element.style.height = 'auto';
   }
-  
+
   void collapse() {
     if (initialAnimSkip) {
       initialAnimSkip = false;
       collapseDone();
-      element.style.height = "0px";
+      element.style.height = "0";
     } else {
       // CSS transitions don't work with height: auto, so we have to manually change the height to a specific value
-      element.style.height = '${element.scrollHeight}px';
+      element.style.height = '${element.children[0].scrollHeight}px';
       //trigger reflow so a browser realizes that height was updated from auto to a specific value
-      var x = element.offsetWidth;
+      var x = element.children[0].offsetWidth;
 
-      element.classes.remove('collapse-in');
-      element.classes.add('collapsing');
+      element.classes
+        ..remove('collapse')
+        ..remove('in')
+        ..add('collapsing');
 
-      doTransition({ 'height': '0' }).then((value)=>collapseDone());
+      doTransition({ 'height': '0' }).then((value) => collapseDone(), onError: (e) {
+        _log.fine('Error on expand: ${e}');
+        collapseDone();
+      });
     }
   }
-  
+
   void collapseDone() {
-    element.classes.remove('collapsing');
-    element.classes.add('collapse');
+    element.classes
+      ..remove('collapsing')
+      ..add('collapse');
   }
 }
