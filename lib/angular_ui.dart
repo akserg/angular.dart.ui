353bc42a092ac76042ad06c9d275e0dfbfd2fe18
zoechi <gzoechi@gmail.com>
2014-01-31 13:13:32 +0100

    add new modules accordion, rating, timeout, transition

--- lib/angular_ui.dart
+++ lib/angular_ui.dart
@@ -5,22 +5,30 @@ library angular.ui;
 
 import "package:angular/angular.dart";
 
+import 'package:angular_ui/alert.dart';
+import 'package:angular_ui/accordion/accordion.dart';
 import 'package:angular_ui/buttons.dart';
+import 'package:angular_ui/carousel.dart';
 import 'package:angular_ui/collapse.dart';
 import 'package:angular_ui/dropdown_toggle.dart';
-import 'package:angular_ui/alert.dart';
-import 'package:angular_ui/carousel.dart';
+import 'package:angular_ui/rating/rating.dart';
+import 'package:angular_ui/timeout.dart';
+import 'package:angular_ui/transition.dart';
 
 /**
  * AngularUI Module
  */
 class AngularUIModule extends Module {
   AngularUIModule() {
+    install(new AccordionModule());
+    install(new AlertModule());
     install(new ButtonModule());
+    install(new CarouselModule());
     install(new CollapseModule());
     install(new DropdownToggleModule());
-    install(new AlertModule());
-    install(new CarouselModule());
+    install(new RatingModule());
+    install(new TimeoutModule());
+    install(new TransitionModule());
   }
 }
 
