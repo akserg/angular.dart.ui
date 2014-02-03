896cee0a56096ac2e1480bd068114afb90df5a77
Sergey Akopkokhyants <akserg@gmail.com>
2014-02-03 17:40:00 +0200

    Added Carousel example.

--- web/main.dart
+++ web/main.dart
@@ -26,6 +26,7 @@ class DemoModule extends Module {
     type(DropdownCtrl);
     type(AlertCtrl);
     type(ProgressCtrl);
+    type(CarouselDemoController);
   }
 }
 
@@ -140,5 +141,41 @@ class ProgressCtrl {
   ProgressCtrl() {
     randomStacked();
     random();
+  }
+}
+
+@NgController(
+    selector: '[ng-controller=carousel-demo-ctrl]',
+    publishAs: 'ctrl')
+class CarouselDemoController {
+
+  // workaround until number conversion is supported by Angular
+  String _myInterval = '5000';
+  String get myIntervalAsString => _myInterval;
+  set myIntervalAsString(String newVal) {
+    _myInterval = newVal;
+    try {
+      myInterval = int.parse(newVal);
+    } catch(e){}
+  }
+  // workaround end
+
+  int myInterval = 5000;
+  List<Map<String,dynamic>> slides = [];
+
+  CarouselDemoController() {
+
+    for (int i = 0; i < 4; i++) {
+      addSlide();
+    }
+  }
+
+  void addSlide() {
+    int newWidth = 600 + slides.length;
+    slides.add({
+      'image': 'http://placekitten.com/${newWidth}/300',
+      'text': ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
   }
 }
