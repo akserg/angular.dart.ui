55b549227e9a8579f15afd053777c4920da21952
Sergey Akopkokhyants <akserg@gmail.com>
2014-10-29 09:48:56 +0200

    Add Travis CI support

--- scripts/travis/run_tests.sh
+++ scripts/travis/run_tests.sh
@@ -9,28 +9,25 @@ echo Fetching dart sdk and Dartium
 curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
 curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
 
-# Unzipping the files
-
+echo Unzipping the files
 unzip $DART_DIST > /dev/null
 unzip $DARTIUM_DIST > /dev/null
 rm $DART_DIST
 rm $DARTIUM_DIST
+
+echo Rename dartium
 mv dartium-* dartium;
 
-# Setting up the environment variables
-
+echo Setting up the environment variables
 export DART_SDK="$PWD/dart-sdk"
 export PATH="$DART_SDK/bin:$PATH"
 export DARTIUM_BIN="$PWD/dartium/chrome"
 
-# Installing all the dependencies
-
+echo Installing all the dependencies
 pub install
 
-# Launching xvfb
-
+echo Launching xvfb
 sh -e /etc/init.d/xvfb start
 
-# Starting Karma
-
+echo Starting Karma
 ./node_modules/karma/bin/karma start --single-run --browsers Dartium
